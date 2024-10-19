fe52c4ee4c93514ed3c28015638c465d926d45ac
Chang Cheng Liou <65118588+aa85469241@users.noreply.github.com>
2024-10-19 16:46:06 +0800

    Update ExpoStoreButton.tsx

--- components/ControlPanel/ExpoStoreButton.tsx
+++ components/ControlPanel/ExpoStoreButton.tsx
@@ -14,7 +14,7 @@ export const ExpoStoreButton = ({
     return (
         <PanelButton id={id} serialNumber={4} className={className}>
             <Link
-                href="https://expo.dev/%40defgh/react-native-typescript?serviceType=eas&distribution=expo-go&scheme=exp%2Breact-native-typescript&channel=production&sdkVersion=48.0.0"
+                href="https://expo.dev/preview/update?message=fixed&updateRuntimeVersion=exposdk%3A51.0.0&createdAt=2024-10-19T08%3A41%3A53.239Z&slug=exp&projectId=79de8a43-baa8-4bc2-9f58-7d83ac8f71ab&group=db80d40e-77d4-430e-a62b-60d415525381"
                 className="absolute w-full h-full rounded-full"
             />
             <div
